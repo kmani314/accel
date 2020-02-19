e220c1c0ba873583e5fb43bcd3ba2a726b9ccae2
Krishna Mani <krishnamani314@gmail.com>
2020-02-19 00:26:10 -0800

    Finish really crappy parser, I only care if this works

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
+mod entry;
+mod tag;
+mod parse;
+
 fn main() {
+    let input: String = "<terms !anki Terms to know></terms ------------->".to_string();
+    let tokens = parse::lex_string(&input);
+    let tags = parse::parse(&tokens);
+    println!("{:?}", tags);
 }
